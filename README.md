90c2a5cb0d454d647f8d2eb06c3ad48bc0773a93
Eric <ehannum@gmail.com>
2016-02-27 21:58:27 -0800

    added very simple data binding

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# bullet-wizards
+# Bullet Wizards Companion
 
 A companion app for the board game Bullet Wizards.
--- src/wizards.js
+++ src/wizards.js
@@ -1,3 +1,13 @@
-var wizard = 'World';
+var game = {
+  set: function (id, value) {
+    var node = document.getElementById(id);
+    if (!node) return;
+    node.textContent = value;
+  },
+  bind: function (id, variable) {
+    this.__variables[variable] = id;
 
-console.log('Hello' + wizard + '!');
+    this.set(id, variable);
+  },
+  __variables: {}
+}
