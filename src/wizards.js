ef469fad8ab413a91b4771f11b60e7c5804ef897
Eric <ehannum@gmail.com>
2016-02-28 03:25:09 -0800

    added title screen

--- src/wizards.js
+++ src/wizards.js
@@ -14,15 +14,12 @@ var binder = {
 
 var page = 0;
 
-document.getElementById('spider').addEventListener('click', function () {
-  alert(document.getElementById('spider').dataset.char);
-});
-
 document.getElementById('prev').addEventListener('click', function () {
   if (page === 0) return;
   page--;
   document.getElementById('book').setAttribute('style', 'margin-left:-' + page*100 + 'vw');
 });
+
 document.getElementById('next').addEventListener('click', function () {
   if (page === 2) return;
   page++;
